78b06561a22d8b8145c7e41b00226fec7d70d035
nienowt <elstad.nienow@gmail.com>
2016-04-06 10:00:09 -0700

    moved file route out of user

--- routes/file-routes.js
+++ routes/file-routes.js
@@ -2,6 +2,7 @@
 
 module.exports = (router) => {
   let File = require(__dirname + '/../models/files-model');
+  let User = require(__dirname + '/../models/users-model');
   let AWS = require('aws-sdk');
   AWS.config.region = 'us-west-2';
   let s3 = new AWS.S3();
@@ -33,5 +34,59 @@ module.exports = (router) => {
           res.end();
         });
       });
+    });
+  router.route('/users/:user/files')
+    .get((req, res) => {
+      User.findOne({_id: req.params.user})
+      .populate('files')
+      .exec((err, user) => {
+        res.json(user.files);
+        res.end();
+      });
+    })
+    .post((req, res) => {
+      let fileName;
+      let fileContent;
+      if (req.headers['content-type'] === 'image/jpeg'){
+        let imgData = [];
+        req.on('data',(data) => {
+          imgData.push(data);
+        }).on('end', ()=>{
+          fileContent = Buffer.concat(imgData);
+          sendFiles();
+        });
+        fileName = req.headers['filename'];
+      } else {
+        fileName = req.body.fileName;
+        fileContent = req.body.content;
+        sendFiles();
+      }
+      function sendFiles(){
+        let bucketName = req.params.user;
+        let s3 = new AWS.S3({params: {Bucket: '401-' + bucketName, Key: fileName}});
+        s3.createBucket(function(err) {
+          if (!err || err.code === 'BucketAlreadyOwnedByYou') {
+            s3.headObject((err, data) => {
+              if (err) console.log('this is the error',err);
+              if (!data){
+                s3.upload({Body: fileContent, ACL: 'public-read'}, (err, data) => {
+                  if (err) console.log('here',err);
+                  res.write('file uploaded');
+                  res.end();
+                  let newFile = new File({bucketId: bucketName, name: fileName, url: data.Location});
+                  newFile.save((err, file) => {
+                    User.findByIdAndUpdate(bucketName, {$push: {'files': file._id}},{'new': true}, (err) => {
+                      if (err) console.log(err);
+                    });
+                  });
+                });
+              }
+            });
+          } else {
+            res.status(400).send('Error:'+ err);
+            res.end();
+          }
+        });
+      }
     });
 };
